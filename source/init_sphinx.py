feb8a0f304c02a9273f963acd3e44d6333b9386f
Thomas Cokelaer <cokelaer@garbarek.(none)>
2011-05-10 13:28:16 +0200

    fix themes directory and version

--- source/init_sphinx.py
+++ source/init_sphinx.py
@@ -1,11 +1,11 @@
-version = '0.3'
+version = '0.9'
 project_name = u'Sphinx and the RST syntax'
 copyright = 'Thomas Cokelaer'
-url = 'http://thomas-cokelaer.info/tutorials/sphinx' 
+url = 'http://thomas-cokelaer.info/tutorials/sphinx/contents.html' 
 authors = 'Thomas Cokelaer'
 title = 'Sphinx and RST syntax guide'
 
 
 #sphinx related
 user_theme = 'software'
-sphinx_themes_path = ['/home/cokelaer/Work/sphinx_themes']
+sphinx_themes_path = ['~/Work/sphinx_themes']
